997de49fbb8b76518e54adf98d5b1ba13df4ec30
Samuel Tull <st862@cam.ac.uk>
2023-12-05 14:23:18 +0000

    3 refactor

--- aoc23/3.py
+++ aoc23/3.py
@@ -3,6 +3,19 @@ from collections import defaultdict, deque, Counter
 import heapq
 import functools  # @functools.lru_cache(maxsize=None)
 
+
+def neighbours(pt):
+    r, c = pt
+    out = []
+    for dr in [-1, 0, 1]:
+        for dc in [-1, 0, 1]:
+            if 0 == dr == dc:
+                continue
+            if 0 <= r + dr < R and 0 <= c + dr < C:
+                out.append((r + dr, c + dc))
+    return out
+
+
 data = "3.txt"
 # data = "3test.txt"
 if len(sys.argv) > 1:
@@ -11,18 +24,15 @@ with open(data) as f:
     data = f.read().strip()
 
 G = []
-P1 = set()
-P2 = {}
+P1 = set()  # all neighbours of symbols- combined
+P2 = []  # all neighbours of * - separate
 
 lines = data.split("\n")
 R = len(lines)
 C = len(lines[0])
 for r in range(R):
-    c = -1
+    c = 0
     while c + 1 < C:
-        c += 1
-        if lines[r][c] == ".":
-            continue
         if lines[r][c] in "0123456789":
             num = lines[r][c]
             pts = [(r, c)]
@@ -31,48 +41,32 @@ for r in range(R):
                 pts.append((r, c + 1))
                 c += 1
             G.append((int(num), pts))
-        else:
-            P1.add((r, c))
+        elif lines[r][c] != ".":
+            neigh = neighbours((r, c))
+            for ri, ci in neigh:
+                P1.add((ri, ci))
             if lines[r][c] == "*":
-                P2[(r, c)] = lines[r][c]
-
-
-def neighbours(pt):
-    r, c = pt
-    for dr in [-1, 0, 1]:
-        for dc in [-1, 0, 1]:
-            if 0 == dr == dc:
-                continue
-            if 0 <= r + dr < R and 0 <= c + dr < C:
-                yield (r + dr, c + dc)
-
+                P2.append(neigh)
+        c += 1
 
 s = 0
-
-for pt in P1:
-    for r, c in neighbours(pt):
-        for i in range(len(G) - 1, -1, -1):
-            (num, pts) = G[i]
-            if (r, c) in pts:
-                s += num
-                del G[i]
+for num, pts in G:
+    if set(pts) & P1:
+        s += num
 print(s)
 
 s = 0
-for pt, val in Gs.items():
-    if val != "*":
-        continue
-    neigh = {}
-    for r, c in neighbours(pt):
-        for i in range(len(G)):
-            num, pts = G[i]
-            if (r, c) in pts:
-                neigh[i] = num
-
-    if len(neigh) == 2:
-        curr = 1
-        for num in neigh.values():
+for neigh in P2:  # all *
+    neigh = set(neigh)
+    count = 0
+    curr = 1
+    for num, pts in G:
+        if neigh & set(pts):
+            if count == 2:  # > 2
+                break
+            count += 1
             curr *= num
-
-        s += curr
+    else:  # no break
+        if count == 2:
+            s += curr
 print(s)
--- aoc23/README.md
+++ aoc23/README.md
@@ -3,8 +3,11 @@ Solving each morning but cannot get up at 5AM for release.
 My (mostly) raw/unmodified solutions - sometimes comment/clean after submitting.  
 Each solution prints the answer to p1 then p2, unless Xp1.py exists.  
 
-## Lessons Learnt  
-# Day 5  
+## Lessons Learnt 
+### Day 3 
+Searching intersections between setA and each of [setI, setJ, setK, ...].  
+Better to store setB = &(I,J,K... ) for fewer comparisons 
+### Day 5  
 Storing intervals as [first, last] much easier to think about than [first, length].  
 Eg. when adding mid we get splits [first, mid], [mid+1, last] requiring no computation of lengths.   
 The choice of inclusive vs half-open slices ([first, last] or [first, last+1]) does not really matter if consistent.  
